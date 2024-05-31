feedf5d917ea0dc1f113679b1c87a48e19cf02e3
Justyn Hunter <10092578+justynhunter@users.noreply.github.com>
2024-05-30 23:00:56 -0500

    Create Index Page (#2)
    Co-authored-by: Justyn Hunter <justynhunter@gmail.com>

--- lib/launchy_web/router.ex
+++ lib/launchy_web/router.ex
@@ -17,7 +17,7 @@ defmodule LaunchyWeb.Router do
   scope "/", LaunchyWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", IndexLive, :home
   end
 
   # Other scopes may use custom stacks.
