84b6bdbe62cb2eb59288b2d9a32a48c0edd0ba9d
Justyn Hunter <justynhunter@gmail.com>
2024-06-01 00:29:28 -0500

    adds changeset to project

--- lib/project.ex
+++ lib/project.ex
@@ -1,5 +1,6 @@
 defmodule Launchy.Project do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "project" do
     field :name, :string
@@ -10,5 +11,11 @@ defmodule Launchy.Project do
     field :updated_at, :utc_datetime
 
     has_many :tasks, Launchy.Task
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :description, :launch_date, :is_complete])
+    |> validate_required([:name, :description])
   end
 end
